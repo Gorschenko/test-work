5fc46fd746a54de749b01c3454b193572cea9b20
Alexey G. <aa.gorschenkO@yandex.ru>
2024-03-15 18:38:01 +0300

    [Users] - Users Module edited

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3010);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+  });
+  await app.init();
 }
+
 bootstrap();
