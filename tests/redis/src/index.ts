79d3d7bb6673597bade0062639e7e309706880ba
Alexey G. <aa.gorschenkO@yandex.ru>
2024-03-29 18:36:48 +0300

    redis test files edited

--- tests/redis/src/index.ts
+++ tests/redis/src/index.ts
@@ -45,3 +45,54 @@ const bootstrap = async () => {
 };
 
 bootstrap();
+
+// КЛЮЧ-ЗНАЧЕНИЕ
+
+// Установка значения в кэше
+// client.set('ключ', 'значение', redis.print);
+
+// Получение значения из кэша
+// client.get('ключ', function(err, reply) {
+//   console.log(reply); // Выводит значение из кэша
+// });
+
+// Установка значения в кэше с таймаутом
+// client.set('ключ', 'значение', 'EX', 10); // Устанавливает значение с таймаутом в 10 секунд
+
+// Удаление значения из кэша
+// client.del('ключ');
+
+// ХЭШ
+
+// Установка значения хеша
+// client.hset('myhash', 'field1', 'value1', redis.print);
+
+// Получение значения из хеша
+// client.hget('myhash', 'field1', function(err, reply) {
+//   console.log(reply); // Выводит значение из хеша
+// });
+
+// Получение всех полей и значений хеша
+// client.hgetall('myhash', function(err, obj) {
+//   console.dir(obj); // Выводит все поля и значения хеша
+// });
+
+// СПИСКИ
+
+// Добавление элементов в список
+// client.rpush('mylist', 'element1', 'element2', redis.print);
+
+// Получение элементов списка
+// client.lrange('mylist', 0, -1, function(err, reply) {
+//   console.log(reply); // Выводит все элементы списка
+// });
+
+// МНОЖЕСТВА
+
+// Добавление элементов во множество
+// client.sadd('myset', 'member1', 'member2', redis.print);
+
+// Получение элементов множества
+// client.smembers('myset', function(err, reply) {
+//   console.log(reply); // Выводит все элементы множества
+// });
