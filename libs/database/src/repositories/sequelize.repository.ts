8e21b6f377b448258f13937fa6cd8dad82ac4bb9
Alexey G. <aa.gorschenkO@yandex.ru>
2024-04-03 12:23:26 +0300

    [Libs] - Sequelize base respository added

--- libs/database/src/repositories/sequelize.repository.ts
+++ libs/database/src/repositories/sequelize.repository.ts
@@ -1,5 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'sequelize-typescript';
+import {
+  BulkCreateOptions,
+  CreateOptions,
+  DestroyOptions,
+  FindOptions,
+  Model,
+  UpdateOptions,
+} from 'sequelize';
 
 @Injectable()
-export class SequelizeRepository<T extends Model> {}
+export class SequelizeRepository<T extends Model<T>> {
+  constructor(protected model: { new (): T } & typeof Model) {}
+
+  async create(attributes: any, options: CreateOptions<T> = {}) {
+    return this.model.create<T>(attributes, options);
+  }
+
+  async bulkCreate(attributes: any, options?: BulkCreateOptions) {
+    return this.model.bulkCreate(attributes, options);
+  }
+
+  async findAll(options: FindOptions<T> = {}) {
+    return this.model.findAll<T>(options);
+  }
+
+  async findOne(options: FindOptions<T>) {
+    return this.model.findOne<T>(options);
+  }
+
+  async findAndUpdate(attributes: Partial<T>, options: UpdateOptions<T>) {
+    return this.model.update<T>(attributes, { ...options, returning: true });
+  }
+
+  async findAndDelete(options: DestroyOptions<T> = {}) {
+    return this.model.destroy<T>(options);
+  }
+}
