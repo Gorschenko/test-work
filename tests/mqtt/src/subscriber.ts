e4f69662cff9529ca50ac20296b967d39ad45a0b
Alexey G. <aa.gorschenkO@yandex.ru>
2024-07-12 09:43:04 +0300

    [mod] - nodemon config edited

--- tests/mqtt/src/subscriber.ts
+++ tests/mqtt/src/subscriber.ts
@@ -3,8 +3,24 @@ import { Url } from './services/types';
 import { mqttSubscriberOptions } from './services/helpers';
 
 const bootstrap = async () => {
-  const subscriber = new MqttService(Url.LOCAL, mqttSubscriberOptions);
+  const subscriber = new MqttService(Url.DEV_RU_BASE, mqttSubscriberOptions);
   await subscriber.subscribeToAllTopics();
 };
 
 bootstrap();
+
+const coordsData = [151, 1, 254, 15, 192, 100, 182, 35, 128, 35, 39, 18, 158, 10, 0, 0, 1];
+console.log('Coords data: ', coordsData);
+console.log('Coords data length: ', coordsData.length);
+
+const coordsBuf = Buffer.from(coordsData);
+console.log('Coords data buffer: ', coordsBuf);
+
+const ptime = coordsBuf.readUInt32LE(0);
+const plat = coordsBuf.readInt32LE(4);
+const plon = coordsBuf.readInt32LE(8);
+const pcourse = coordsBuf.readUInt16LE(12);
+const pspeed = coordsBuf.readUInt16LE(14);
+const pmode = coordsBuf.readUInt8(16);
+
+console.log('Parsed coords data: ', ptime, plat, plon, pcourse, pspeed, pmode);
