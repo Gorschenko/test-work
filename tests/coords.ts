110b24fff8456915e54118af1d786536729647ab
Alexey G. <aa.gorschenkO@yandex.ru>
2024-07-12 11:01:56 +0300

    [mod] - test files edited

---
+++ tests/coords.ts
@@ -0,0 +1,51 @@
+// Парсинг события /n и перевод данных в бинарник
+// [200,43,254,15,0,99,182,35,192,33,39,18,137,7,0,0,1]
+const coordsData = [151, 1, 254, 15, 192, 100, 182, 35, 128, 35, 39, 18, 158, 10, 0, 0, 1];
+
+export interface IParsedCoord {
+  ptime: number;
+  plat: number;
+  plon: number;
+  pcourse: number;
+  pspeed: number;
+  pmode: number;
+}
+
+export const parse = (coordsData: number[]): IParsedCoord => {
+  console.log('Coords data: ', coordsData, 'length: ', coordsData.length);
+  const coordsBuf = Buffer.from(coordsData);
+
+  const ptime = coordsBuf.readUInt32LE(0);
+  const plat = coordsBuf.readInt32LE(4);
+  const plon = coordsBuf.readInt32LE(8);
+  const pcourse = coordsBuf.readUInt16LE(12);
+  const pspeed = coordsBuf.readUInt16LE(14);
+  const pmode = coordsBuf.readUInt8(16);
+
+  return {
+    ptime,
+    plat,
+    plon,
+    pcourse,
+    pspeed,
+    pmode,
+  };
+};
+
+const parsed = parse(coordsData);
+console.log('Parsed coords data: ', parsed);
+
+export const serialize = (coord: IParsedCoord): Buffer => {
+  const buffer = Buffer.alloc(17);
+  buffer.writeUInt32LE(coord.ptime, 0);
+  buffer.writeInt32LE(coord.plat, 4);
+  buffer.writeInt32LE(coord.plon, 8);
+  buffer.writeUInt16LE(coord.pcourse, 12);
+  buffer.writeUInt16LE(coord.pspeed, 14);
+  buffer.writeInt8(coord.pmode, 16);
+  return buffer;
+};
+
+const serialized = serialize(parsed);
+
+console.log(serialized);
---
+++ tests/lbs.ts
@@ -0,0 +1,14 @@
+// Изменить	dc_id	dc_dev_id	dc_dev_login	dc_time	dc_longitude	dc_latitude	dc_course	dc_speed	dc_accuracy	dc_mode	dc_created
+// редактировать	97115193	200105	9990002	269112447	302123360	600037410	0	0	NULL	0	2024-07-11 20:27:40
+/* 
+longitude 303963358, 9 знаков
+latitud 599175000, 9 знаков
+*/
+
+const lbs = {
+  location: {
+    lat: 49.9571529,
+    lng: 15.278397,
+  },
+  accuracy: 1000.0,
+};
