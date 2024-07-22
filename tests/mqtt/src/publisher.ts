e4131634cab7cd5a4cb3c0a691feb0b88f5dc08d
Alexey G. <aa.gorschenkO@yandex.ru>
2024-07-22 15:20:38 +0300

    [mod] - tests files edited

--- tests/mqtt/src/publisher.ts
+++ tests/mqtt/src/publisher.ts
@@ -2,22 +2,23 @@ import { mqttPublisherOptions } from './services/helpers';
 import { Topic, Url } from './services/types';
 import { MqttDevicePublisher } from './services/MqttDevicePublisher';
 import { createPacket } from './parsers/lbs/createPacket';
-import { createTowersArray } from './parsers/lbs/helpers';
+// import { createTowersArray } from './parsers/lbs/helpers';
+import { ITower } from './parsers/lbs/types';
 
 const bootstrap = () => {
-  const publisher = new MqttDevicePublisher(Url.LOCAL, mqttPublisherOptions);
+  const publisher = new MqttDevicePublisher(Url.DEV_RU_BASE, mqttPublisherOptions);
   setInterval(async () => {
-    const towers = createTowersArray();
+    // const towers = createTowersArray();
 
-    // const towers = [
-    //   {
-    //     radioType: 'gsm',
-    //     mobileCountryCode: 208,
-    //     mobileNetworkCode: 1,
-    //     locationAreaCode: 2,
-    //     cellId: 1234567,
-    //   },
-    // ];
+    const towers: ITower[] = [
+      {
+        radioType: 'gsm',
+        mobileCountryCode: 208,
+        mobileNetworkCode: 1,
+        locationAreaCode: 2,
+        cellId: 1234567,
+      },
+    ];
 
     const packet = createPacket(towers);
     await publisher.publish(Topic.REQUESTS, packet);
--- tests/mqtt/src/subscriber.ts
+++ tests/mqtt/src/subscriber.ts
@@ -3,7 +3,7 @@ import { Url } from './services/types';
 import { mqttSubscriberOptions } from './services/helpers';
 
 const bootstrap = async () => {
-  const subscriber = new MqttService(Url.LOCAL, mqttSubscriberOptions);
+  const subscriber = new MqttService(Url.DEV_RU_BASE, mqttSubscriberOptions);
   await subscriber.subscribeToAllTopics();
 };
 
