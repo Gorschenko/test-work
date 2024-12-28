3ef2b2ddf608abdd619640aef98493b5918cb7e6
Alexey G <alaexey.gor@safit.pro>
2024-12-28 15:59:42 +0300

    mod: edit parser

--- tests/parse/src/index.ts
+++ tests/parse/src/index.ts
@@ -1,5 +1,10 @@
-import { COMPASS_SCHEMA, get_0x40_igla_schema, GET_SYS_INFO_TO_BINARY } from './static';
+import {
+  COMPASS_SCHEMA,
+  get_0x40_igla_schema,
+  GET_SYS_INFO_TO_BASE64,
+  GET_SYS_INFO_TO_BINARY,
+} from './static';
 import { get_public_compass_packet, get_public_igla_packet, parse_packet } from './utils';
 
-const parsed_packet = parse_packet(GET_SYS_INFO_TO_BINARY, get_0x40_igla_schema());
-const public_parsed_packet = get_public_igla_packet(parsed_packet);
+const parsed_packet = parse_packet(GET_SYS_INFO_TO_BASE64, COMPASS_SCHEMA);
+const public_parsed_packet = get_public_compass_packet(parsed_packet);
